7fcc6fa97283e9d71bd83b9a9055b5542262cda6
srinivassankoji <srinivaschary.chary@gmail.com>
2021-08-25 12:29:30 +0530

    Moved Spring IOC

--- IOC/src/main/java/com/ioc/dzone/service/QuizMaster.java
+++
@@ -1,6 +0,0 @@
-package com.ioc.dzone.service;
-
-public interface QuizMaster {
-
-	public String popquestion();
-}
--- IOC/src/main/java/com/ioc/dzone/service/StrutsQuizMaster.java
+++
@@ -1,9 +0,0 @@
-package com.ioc.dzone.service;
-
-public class StrutsQuizMaster implements QuizMaster {
-
-	@Override
-	public String popquestion() {
-		return "Are you new to Struts ?";
-	}
-}
